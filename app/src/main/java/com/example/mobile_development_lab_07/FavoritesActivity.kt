64ec0c6e5b1dcd716044cf09cfe348112e001dc8
tommykorch <tkorchmit@bk.ru>
2024-12-19 00:29:24 +0700

    Part 12

--- app/src/main/java/com/example/mobile_development_lab_07/FavoritesActivity.kt
+++ app/src/main/java/com/example/mobile_development_lab_07/FavoritesActivity.kt
@@ -9,15 +9,15 @@ class FavoritesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites)
-        // Проверяем, пуст ли контейнер для фрагмента (savedInstanceState будет null при первом создании активности)
+
         val isFragmentContainerEmpty = savedInstanceState == null
 
-        // Если контейнер пуст, добавляем новый фрагмент PhotoGalleryFragment
+
         if (isFragmentContainerEmpty) {
             supportFragmentManager // Получаем экземпляр FragmentManager для управления фрагментами
                 .beginTransaction() // Начинаем транзакцию фрагментов
                 .add(R.id.container, FavoritesFragment.newInstance()) // Добавляем новый экземпляр фрагмента в контейнер
-                .commit() // Подтверждаем транзакцию
+                .commit()
         }
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
